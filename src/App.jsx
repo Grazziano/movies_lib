f855d80c5d39aad192e0c2cb1940edcfdca724e0
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-07-10 20:30:16 -0300

    Component Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Navbar />
       <h2>Movies Lib</h2>
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
