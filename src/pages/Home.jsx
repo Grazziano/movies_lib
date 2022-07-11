9c56e7b8a190247fb68c0035b7be20f25c73ebd7
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-07-10 21:13:21 -0300

    Component MovieCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,21 @@
 import React, { useEffect, useState } from 'react';
 
+import MovieCard from '../components/MovieCard';
+
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 export default function Home() {
-  // console.log(moviesURL, apiKey);
   const [topMovies, setTopMovies] = useState([]);
 
   const getTopRateMovies = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
-    // console.log(data);
     setTopMovies(data.results);
   };
 
   useEffect(() => {
     const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
-    // console.log(topRatedUrl);
     getTopRateMovies(topRatedUrl);
   }, []);
 
@@ -26,9 +25,7 @@ export default function Home() {
       <div className="movies-container">
         {topMovies.length === 0 && <p>Carregando...</p>}
         {topMovies.length > 0 &&
-          topMovies.map((movie) => {
-            return <p>{movie.title}</p>;
-          })}
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
     </div>
   );
